32532c662e6fcab7e967dc27779cd26df115b899
Atimapre Goodness <atimaxgoodness@gmail.com>
2025-01-31 21:12:23 +0100

    made changes

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,42 @@ const hItems = document.querySelector('.items');
 const arrowUp = document.querySelector('.arrow');
 const body = document.querySelector('body');
 
+window.addEventListener('scroll', reveal);
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 100;
+
+    if (revealTop < windowheight - revealPoint) {
+      reveals[i].classList.add('active');
+    }
+    else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', revealRight);
+function revealRight() {
+  var reveals = document.querySelectorAll('.revealRight');
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 100;
+
+    if (revealTop < windowheight - revealPoint) {
+      reveals[i].classList.add('active');
+    }
+    else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+
 hTitle.addEventListener('mouseover', () => {
   hItems.classList.add('active');
   arrowUp.classList.add('arrowDown');
