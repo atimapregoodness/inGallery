85dae16c20a9ba1839bbb9b7dbd7a608b3ff3851
Atimapre Goodness <atimaxgoodness@gmail.com>
2025-02-08 18:52:47 +0100

    upgraded preview page

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
   console.log('DOM fully loaded and parsed');
 
+
+  const zoomInButton = document.querySelector('#zoomIn');
+  const zoomOutButton = document.querySelector('#zoomOut');
+  const zoomableImage = document.querySelector('#zoomableImage');
+  const zoomInOut = document.querySelector('#zoomInOut');
+  const resetBtn = document.querySelector('#resetBtn');
+
+  if (zoomInButton && zoomOutButton && zoomableImage && zoomInOut && resetBtn) {
+    const scaleFactor = 30;
+    const originalSize = 15;
+
+
+    zoomInOut.addEventListener('change', function () {
+      zoomInValue = zoomInOut.value;
+      zoomableImage.style.width = `${zoomInOut.value}em`;
+    });
+
+    zoomInButton.addEventListener('click', function () {
+      zoomInOut.value = parseInt(zoomInOut.value) + scaleFactor;
+
+      zoomableImage.style.width = `${zoomInOut.value}em`;
+    });
+
+    zoomOutButton.addEventListener('click', function () {
+      zoomInOut.value = Math.max(15, zoomInOut.value - scaleFactor); // Prevent scaling below the original size
+      zoomableImage.style.width = `${zoomInOut.value}em`;
+    });
+
+    resetBtn.addEventListener('click', () => {
+      zoomInOut.value = originalSize;
+      zoomableImage.style.width = `${originalSize}em`;
+
+    });
+  }
+
+
+
   const popUpBtn = document.querySelector('.popUpBtn');
   const popUpBox = document.querySelector('.popUpBox');
   const closeButton = document.querySelector('#closeButton');
@@ -17,6 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
       popUpBox.classList.remove('activeBox');
     });
   }
+
 
   window.addEventListener('scroll', reveal);
   function reveal() {
