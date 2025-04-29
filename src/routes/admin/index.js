e7879b98f2a587e303ae5e60955f604a871fbe8f
Atimapre Goodness <atimaxgoodness@gmail.com>
2025-04-29 10:15:57 +0100

    made changes

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const isAdmin = require("../../middleware/isAdmin");
+const redirect = require("../../middleware/redirect");
 
-router.get("/", isAdmin, (req, res) => {
+router.get("/", isAdmin, redirect, (req, res) => {
   // res.render("admin/dashboard");
   res.send("Welcome admin");
 });
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -6,6 +6,7 @@ const profileRoutes = require("../user/users/dashboardRoutes");
 const authRoutes = require("../user/users/authRoutes");
 const exploreRoutes = require("../user/exploreRoutes");
 const homeRoutes = require("./homeRoutes");
+const redirect = require("../../middleware/redirect");
 
 // Use routes
 router.use("/", homeRoutes);
